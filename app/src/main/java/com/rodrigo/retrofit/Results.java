431d5fc2980d12a02f133f94b909483bde3bf2da
Rodrigo Lima <rodrigootavio19@gmail.com>
2023-05-05 20:40:29 -0400

    Initial commit

---
+++ app/src/main/java/com/rodrigo/retrofit/Results.java
@@ -0,0 +1,13 @@
+package com.rodrigo.retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Results {
+
+    @SerializedName("name")
+    private String superName;
+
+    public Results(String name) { this.superName = name;}
+
+    public String getName() { return  superName; }
+}
